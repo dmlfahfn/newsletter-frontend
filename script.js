7633cfca5a68b367a763376197596f4663391c98
Durre shewar Malik <durreshewarmalik@Durres-MacBook-Pro.local>
2021-04-20 22:34:24 +0200

    displaying users

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let firstPage = document.getElementById("first-page");
-let adminPage = document.getElementById("admin-page");
+const firstPage = document.getElementById("first-page");
+const adminPage = document.getElementById("admin-page");
 const header = document.getElementsByTagName("header");
 
 fetch("http://localhost:3000/users")
 .then(res => res.json())
 .then(data => {console.log(data)
 
-    beginningPage(data)
+    showUsers(data)
 });
 
-function beginningPage(data) {
-    firstPage.insertAdjacentHTML("afterbegin", `<form action="" id="register">
-                                                <input type="text" >
-                                                <input type="password">
-                                                <button>Register</button></form>
-                                                <form action="" id="member">
-                                                <input type="text" >
-                                                <input type="password">
-                                                <button>Log In</button></form>`);
+function showUsers(users) {
+     firstPage.insertAdjacentHTML("beforeend", `<form action="" id="register">
+                                                 User Name <input type="text" >
+                                                 Password <input type="password"> 
+                                                 <button>Register</button></form>
+                                                 <form action="" id="member">
+                                                 User Name <input type="text" >
+                                                 Password <input type="password"> 
+                                                 <button>Log In</button></form>`);
 
+    firstPage.insertAdjacentHTML("beforeend","<div><h2>Users in the system</h2></div>");
+    
+    for (user in users){
+        firstPage.insertAdjacentHTML("beforeend", "<div>" + users[user].username + "</div>")
+    }
 };
-beginningPage();
+
+
+showUsers();
