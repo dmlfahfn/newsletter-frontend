ad5552d607988d95f2e3c3e650bf9be98ff87183
Durre shewar Malik <durreshewarmalik@Durres-MacBook-Pro.local>
2021-04-21 22:32:34 +0200

    fron end not recieving data.username from backend

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const firstPage = document.getElementById("first-page");
-const adminPage = document.getElementById("admin-page");
+const secondPage = document.getElementById("second-page");
 const header = document.getElementsByTagName("header");
 let saveBtn = document.getElementById("saveBtn");
 let logInBtn = document.getElementById("logInBtn");
 
-fetch("http://localhost:3000/users")
-.then(res => res.json())
-.then(data => {console.log(data)
+// showUsers();
 
-    showUsers(data)
-});
-
-function showUsers(users) {
-    firstPage.insertAdjacentHTML("beforeend","<div><h2>Our Users</h2></div>");
-    
-    for (user in users){
-        firstPage.insertAdjacentHTML("beforeend", "<div>" + users[user].username + "</div>")
-    }
-};
+// function showUsers() {
+//     firstPage.insertAdjacentHTML("beforeend","<div><h2>Welcome Dear Users</h2></div>");
+//     fetch("http://localhost:3000/users")
+//     .then(res => res.json())
+//     .then(data => {console.log(data)
+//         for (user in data){
+//             firstPage.insertAdjacentHTML("beforeend", "<div>" + data[user].username + "</div>")
+//         }
+//     });
+// };
 
 saveBtn.addEventListener("click", ()=> {
     let newUser = {username: document.getElementById("newUsername").value, password: document.getElementById("newPassword").value, subscription: false };
@@ -49,7 +47,6 @@ logInBtn.addEventListener("click", () => {
     })
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        console.log(data)//.username, data.subscription
     });
-
 });
